d717da871beb9cfca1773d47a376825caabc3d60
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-17 13:04:44 +0600

    commit homework

---
+++ script.js
@@ -0,0 +1,38 @@
+// let a = [1, 2, 3, 0, 4, 5, 6]
+// console.log(a.map(el => el*el ))
+
+// let a = [1, 2, 3, 0, 4, 5, 6]
+// console.log(a.map(el => el > 0 ? true: false))
+
+// let a = [1, 2, -3, 0, 4, -5, 6]
+// console.log(a.filter(el => el > 0))
+
+// let a = [1, 2, -3, 0, 4, -5, 6]
+// console.log(a.filter(el => el < 0))
+
+// let a = [1, -2, 3, 0, 4, -5, 6]
+// console.log(a.filter(el => el % 2 ===0))
+
+// let a = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun ','even']
+// console.log(a.filter(el => el.length > 5 ))
+
+// let a = [1, 2, -3, 0, -4, 5, -6]
+// let b = 0
+// let c = a.map(el => el < 0 ? b += 1 : null )
+// console.log(b)
+
+// let a = [1,2,3,4]
+// let b =  ['a','b','c','d']
+// console.log([...a , ...b])
+
+// let a = [2, 3, 4, 5]
+// let b = 'hello world'
+// console.log([...a , b] )
+
+// let a = [2, 3, 4, 5]
+// let b = 'hello world'
+// console.log([...a , ...b.replace(' ', '')] )
+
+// let a = [2, 3, 4, 5]
+// let b = 'hello world'
+// console.log([...a , ...b.replace(' ', '') , ...a ] )
